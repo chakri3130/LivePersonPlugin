d574b402e485ff98223805b25bcffee94acd35a8
chakri3130 <amar.chakri426@gmail.com>
2021-01-27 08:23:22 +0530

    Updated LivePersonPlugin.m file

--- src/ios/Integration/LivePersonPlugin.m
+++ src/ios/Integration/LivePersonPlugin.m
@@ -1,36 +1,40 @@
-// /********* LivePersonPlugin.m Cordova Plugin Implementation *******/
-
 // #import <Cordova/CDV.h>
+// #import "QMC_Health_ID-Swift.h"
+// #import <UIKit/UIKit.h>
 
 // @interface LivePersonPlugin : CDVPlugin {
 //   // Member variables go here.
 // }
+// // - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+// - (void)ConnectToBot:(CDVInvokedUrlCommand*)command;
 
-// - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+// @property (strong, nonatomic) UINavigationController* lpNavigationController;
+
 // @end
 
 // @implementation LivePersonPlugin
 
-// - (void)coolMethod:(CDVInvokedUrlCommand*)command
+
+// - (void)ConnectToBot:(CDVInvokedUrlCommand*)command
 // {
-//     CDVPluginResult* pluginResult = nil;
-//     NSString* echo = [command.arguments objectAtIndex:0];
 
-//     if (echo != nil && [echo length] > 0) {
-//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-//     } else {
-//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-//     }
+//     LPMessagingWrapper* lpMessagingWrapper =  [[LPMessagingWrapper alloc]init];
+//     [lpMessagingWrapper showChat];
 
-//     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-// }
+// // NSDictionary *chatBotDict = [[NSDictionary alloc]initWithObjectsAndKeys:@"firstName",[command.arguments objectAtIndex:0],@"lastName",[command.arguments objectAtIndex:1],@"environment", [command.arguments objectAtIndex:2],@"brandID", [command.arguments objectAtIndex:3],@"mode", [command.arguments objectAtIndex:4],@"authToken", [command.arguments objectAtIndex:5]//
+         
+//  }
+
+
+
+
+
 
 // @end
-/********* LivePerson.m Cordova Plugin Implementation *******/
-
 #import <Cordova/CDV.h>
 #import "QMC_Health_ID-Swift.h"
 #import <UIKit/UIKit.h>
+#import <LPMessagingSDK/LPMessagingSDK.h>
 
 @interface LivePersonPlugin : CDVPlugin {
   // Member variables go here.
@@ -39,26 +43,74 @@
 - (void)ConnectToBot:(CDVInvokedUrlCommand*)command;
 
 @property (strong, nonatomic) UINavigationController* lpNavigationController;
-
+@property (strong, nonatomic)LPMessagingWrapper* lpMessagingWrapper;
 @end
 
 @implementation LivePersonPlugin
 
 
-- (void)ConnectToBot:(CDVInvokedUrlCommand*)command
-{
+- (void)ConnectToBot:(CDVInvokedUrlCommand*)command {
 
-    LPMessagingWrapper* lpMessagingWrapper =  [[LPMessagingWrapper alloc]init];
-    [lpMessagingWrapper showChat];
+    if (self.lpMessagingWrapper == nil) {
+        //Set user
+        LPUser* user = [[LPUser alloc]initWithFirstName: @""
+                                               lastName: @""
+                                               nickName: @""
+                                                    uid: @""
+                                        profileImageURL: @""
+                                            phoneNumber: @""
+                                             employeeID: @""];
 
-// NSDictionary *chatBotDict = [[NSDictionary alloc]initWithObjectsAndKeys:@"firstName",[command.arguments objectAtIndex:0],@"lastName",[command.arguments objectAtIndex:1],@"environment", [command.arguments objectAtIndex:2],@"brandID", [command.arguments objectAtIndex:3],@"mode", [command.arguments objectAtIndex:4],@"authToken", [command.arguments objectAtIndex:5]//
-         
+        //Set Auth
+        LPAuthenticationParams* authParams =
+        [[LPAuthenticationParams alloc] initWithAuthenticationCode: @""
+                                                               jwt: @""
+                                                       redirectURI: @""
+                                             certPinningPublicKeys: @[@"", @""]
+                                                authenticationType: LPAuthenticationTypeSignup];
+
+        self.lpMessagingWrapper = [[LPMessagingWrapper alloc] initWithUser: nil
+                                                      authenticationParams: nil];
+    }
+
+    //Login SDK
+
+    //Logougt SDK
+
+    //Clear History
+    
+    //Set engagement with entry point example (callback):
+    /**
+        ios-welcome, prodm, us, en
+     */
+    NSDictionary* entryPoint = @{@"entryPoint" : @"ios-welcome",
+                                 @"environment" : @"prod",
+                                 @"locale" : @"us",
+                                 @"language" : @"en"};
+
+
+    //show chat (after success callback from engagement:
+    [self.lpMessagingWrapper showChat];
+
  }
 
-
-
-
-
-
 @end
+
+
+// NSDictionary *chatBotDict = [[NSDictionary alloc]initWithObjectsAndKeys:@"firstName",[command.arguments objectAtIndex:0],@"lastName",[command.arguments objectAtIndex:1],@"environment", [command.arguments objectAtIndex:2],@"brandID", [command.arguments objectAtIndex:3],@"mode", [command.arguments objectAtIndex:4],@"authToken", [command.arguments objectAtIndex:5]//
+
+//     dispatch_async(dispatch_get_main_queue(), ^{
+//         UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+//         self.lpNavigationController = self.lpNavigationController ? :
+//         [sb instantiateViewControllerWithIdentifier:@"LPNavigation"];
+//
+//        // LPMessagingSwift *vc = [sb instantiateViewControllerWithIdentifier:@"LPMessagingSwift"];
+//         self.lpNavigationController.modalPresentationStyle = UIModalPresentationFullScreen;
+//         [self.viewController presentViewController:self.lpNavigationController
+//                                           animated:YES completion:^{
+//             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ok"];
+//             // [vc connectToRoom:room];
+//             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//         }];
+//     });
 
