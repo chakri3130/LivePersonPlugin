0b1547a1f81a231e102ada008c7b99df8ab29406
chakri3130 <amar.chakri426@gmail.com>
2021-01-29 11:46:21 +0530

    Update LivePersonPlugin.js

--- www/LivePersonPlugin.js
+++ www/LivePersonPlugin.js
@@ -3,6 +3,6 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'LivePersonPlugin', 'coolMethod', [arg0]);
 };
-exports.ConnectToBot = function (firstName,BRAND_ID,APP_INSTALLATION_ID,APP_ID,ISSUER,EntryPoint,EntryPoint_environment,EntryPoint_country,EntryPoint_language,success, error) {
-    exec(success, error, 'LivePersonPlugin', 'ConnectToBot', [firstName,BRAND_ID,APP_INSTALLATION_ID,APP_ID,ISSUER,EntryPoint,EntryPoint_environment,EntryPoint_country,EntryPoint_language]);
+exports.ConnectToBot = function (firstName,BRAND_ID,APP_INSTALLATION_ID,APP_ID,ISSUER,entryPoint,entryPoint_Environment,entryPoint_Country,entryPoint_language,success, error) {
+    exec(success, error, 'LivePersonPlugin', 'ConnectToBot', [firstName,BRAND_ID,APP_INSTALLATION_ID,APP_ID,ISSUER,entryPoint,entryPoint_Environment,entryPoint_Country,entryPoint_language]);
 };
