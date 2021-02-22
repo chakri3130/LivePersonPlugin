412ca9b4fc60d8e4104a2fb16e2653f79e3f94ab
chakri3130 <amar.chakri426@gmail.com>
2021-02-22 14:21:30 +0530

    Update LivePersonPlugin.js

--- www/LivePersonPlugin.js
+++ www/LivePersonPlugin.js
@@ -11,3 +11,7 @@ exports.instantiateLPMessagingSDK = function([arg0],success, error){
 exports.ConnectToBot = function (entryPoint_Environment,entryPoint_language,entryPoint,entryPoint_Country,success, error) {
     exec(success, error, 'LivePersonPlugin', 'ConnectToBot', [entryPoint_Environment,entryPoint_language,entryPoint,entryPoint_Country]);
 };
+
+exports.showChat = function(deviceToken,success, error){
+    exec(success,error,'LivePersonPlugin', 'showChat', [deviceToken] )
+}
